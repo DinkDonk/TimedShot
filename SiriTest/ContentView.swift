8fb98f9e2f6d6434ffd668472496d789dc2e5cf5
Rune Warhuus <rune@dinkdonk.no>
2021-05-08 15:52:41 +0200

    Initial Commit

---
+++ SiriTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SiriTest
+//
+//  Created by Rune Warhuus on 08/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SiriTest/SiriTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiriTestApp.swift
+//  SiriTest
+//
+//  Created by Rune Warhuus on 08/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SiriTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
